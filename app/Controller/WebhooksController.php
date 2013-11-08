b13e83ba9123e12f595d996cb403a63f5f2d41aa
Anurag <anurag.pattnaik@andolasoft.com>
2013-11-08 19:41:14 +0530

    test for webhook
    Signed-off-by: Anurag <anurag.pattnaik@andolasoft.com>

--- app/Controller/WebhooksController.php
+++ app/Controller/WebhooksController.php
@@ -4,9 +4,18 @@ class WebhooksController extends AppController {
 
 	function updategit()
 	{
-		shell_exec('cd /home/andolarh/public_html');
-		$output = shell_exec('git pull');
-		echo "<pre>$output</pre>";
+		//shell_exec('cd /home/andolarh/public_html');
+		//$output = shell_exec('git pull');
+		//echo "<pre>$output</pre>";
+		//exit;
+		
+		$connection = ssh2_connect('shell.andola.revhero.com', 22);
+		
+		if (ssh2_auth_password($connection, 'andolarh', 'rev123')) {
+		  echo "Authentication Successful!\n";
+		} else {
+		  die('Authentication Failed...');
+		}
 		exit;
 	}
 }
