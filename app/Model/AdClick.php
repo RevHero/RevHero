dc06b21eb04ac1d555934af29e475436ab0c60dd
Anurag <anurag.pattnaik@andolasoft.com>
2013-12-03 16:38:22 +0530

    Changed the functionality for the line chart on the view stats page
    Signed-off-by: Anurag <anurag.pattnaik@andolasoft.com>

--- app/Model/AdClick.php
+++ app/Model/AdClick.php
@@ -17,11 +17,11 @@ class AdClick extends AppModel {
 		//$getclickdetails = $this->query('SELECT count(id) as clickCount, `placement_id`, created FROM `ad_clicks` where `placement_id`=16 group by DATE_FORMAT(`created`, "%Y-%m-%d") ');
 		if($status == 0)
 		{
-			$clickdetails = $this->find('all', array('fields'=>array('COUNT(`AdClick`.`id`) as clickCount', 'AdClick.placement_id', 'AdClick.created'),'conditions'=>array('AdClick.placement_id'=>$placementId, 'AdClick.is_duplicate'=>0), 'group' => 'DATE_FORMAT(`AdClick`.`created`,"%Y-%m-%d")'));
+			$clickdetails = $this->find('all', array('fields'=>array('COUNT(`AdClick`.`id`) as clickCount', 'AdClick.placement_id', 'AdClick.created'),'conditions'=>array('AdClick.placement_id'=>$placementId, 'AdClick.is_duplicate'=>0), 'order' => '`AdClick`.`created` DESC', 'group' => 'DATE_FORMAT(`AdClick`.`created`,"%Y-%m-%d")', 'limit'=>'7'));
 		}
 		else
 		{
-			$clickdetails = $this->find('all', array('fields'=>array('COUNT(`AdClick`.`id`) as clickCount', 'AdClick.placement_id', 'AdClick.created'),'conditions'=>array('AdClick.placement_id'=>$placementId), 'group' => 'DATE_FORMAT(`AdClick`.`created`,"%Y-%m-%d")'));
+			$clickdetails = $this->find('all', array('fields'=>array('COUNT(`AdClick`.`id`) as clickCount', 'AdClick.placement_id', 'AdClick.created'),'conditions'=>array('AdClick.placement_id'=>$placementId), 'order' => '`AdClick`.`created` DESC', 'group' => 'DATE_FORMAT(`AdClick`.`created`,"%Y-%m-%d")', 'limit'=>'7'));
 		}	
 		return $clickdetails;
 	}
