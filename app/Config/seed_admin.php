f13bb81ff1d46e67b5847409028d5af1e3c40d27
Anurag <anurag.pattnaik@andolasoft.com>
2013-12-09 12:38:29 +0530

    Updated code in seed_admin.php for configuring the database changes
    Signed-off-by: Anurag <anurag.pattnaik@andolasoft.com>

--- app/Config/seed_admin.php
+++ app/Config/seed_admin.php
@@ -2,11 +2,11 @@
 $admin_email = 'test@admin.com';
 $admin_pass  = 'test123';
 $host_name = "localhost";
-$user_name = "root";
-$user_password = "";
-$db_name = "rev_sand";
+$DB_user_name = "root";
+$DB_user_password = "";
+$db_name = "rev_final";
 
-$conn = mysqli_connect($host_name, $user_name, $user_password, $db_name);
+$conn = mysqli_connect($host_name, $DB_user_name, $DB_user_password, $db_name);
 
 $selectData = mysqli_query($conn, "select * from `users` where `admin`=1");
 $num = mysqli_num_rows($selectData);
